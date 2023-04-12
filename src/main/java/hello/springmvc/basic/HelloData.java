c6df3497a51a7a7f0daaf6855b6189d8462c259e
soyeonvv <sovvworld@gmail.com>
2023-04-12 18:43:04 +0900

    HTTP 요청 파라미터 - JSON

--- src/main/java/hello/springmvc/basic/HelloData.java
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -1,22 +1,9 @@
 package hello.springmvc.basic;
 
+import lombok.Data;
+
+@Data
 public class HelloData {
     private String username;
     private int age;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
